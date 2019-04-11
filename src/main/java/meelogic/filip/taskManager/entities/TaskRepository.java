b82673cb3242722a2b472939f69247d3eba8ce5d
Filip Mucko <mucko.fil@ad.meelogic.com>
2019-04-11 15:47:14 +0200

    TaskRepository prepared for movieng to MySQL database

--- src/main/java/meelogic/filip/taskManager/entities/TaskRepository.java
+++ src/main/java/meelogic/filip/taskManager/entities/TaskRepository.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-//docelowo ten moduł ma obsługiwać bazę danych więc dobrze byłoby go budować tak, żeby tylko on musiał być zmieniony gdy do tego dojdzie
+//tutaj będzie miało miejsce łącznie się z bazą danych i parsowanie encji na listę, która będzie shareowana dalej
+//tym sposobem jest to jedyna klasa wymagająca przebudowy przy wdrażaniu bazy danych
 @Repository
 public class TaskRepository {
 
@@ -14,21 +15,5 @@ public class TaskRepository {
 
     public List<Task> getTaskList() {
         return this.taskList;
-    }
-
-    public void removeById(Integer id) {
-        this.taskList.removeIf(task -> task.getId().equals(id));
-    }
-
-    public void add(Task task) {
-        this.taskList.add(task);
-    }
-
-    public void renameTask(String newName, Integer id) {
-        for (Task task : this.taskList) {
-            if(task.getId().equals(id)){
-                task.setName(newName);
-            }
-        }
     }
 }
