152f7623b32ff43d306d65d8ea296dcc00506fbb
Filip Mucko <mucko.fil@ad.meelogic.com>
2019-04-23 11:18:39 +0200

    implemented flyway using flyway object

--- src/main/java/meelogic/filip/taskManager/TaskManagerApplication.java
+++ src/main/java/meelogic/filip/taskManager/TaskManagerApplication.java
@@ -1,5 +1,6 @@
 package meelogic.filip.taskManager;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,9 @@ import java.util.List;
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
+        Flyway flyway = new Flyway();
+        flyway.setDataSource("jdbc:mysql://10.6.4.172:3306/taskManager","root","supersecret");
+        flyway.migrate();
         SpringApplication.run(TaskManagerApplication.class, args);
     }
 }
