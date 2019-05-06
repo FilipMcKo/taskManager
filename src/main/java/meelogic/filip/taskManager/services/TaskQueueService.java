ad5eb0c3bbf2bc70278accd226cac49281d7c5dc
Filip Mucko <mucko.fil@ad.meelogic.com>
2019-05-06 13:15:12 +0200

    connection problems

--- src/main/java/meelogic/filip/taskManager/services/TaskQueueService.java
+++ src/main/java/meelogic/filip/taskManager/services/TaskQueueService.java
@@ -2,6 +2,9 @@ package meelogic.filip.taskManager.services;
 
 import com.rabbitmq.client.*;
 import meelogic.filip.taskManager.entities.internal.Task;
+import org.apache.logging.slf4j.SLF4JLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,22 +15,32 @@ import java.util.concurrent.TimeoutException;
 public class TaskQueueService {
 
     private final static String QUEUE_NAME = "Finished tasks";
-
+    Logger logger = LoggerFactory.getLogger(SLF4JLogger.class);
+/*
     @Autowired
-    ConnectionFactory factory;
+    ConnectionFactory factory;*/
 
-    public void publishToQueue(Task task) {
-        try (Connection connection = factory.newConnection()
-            ) {
+    void publishToQueue(Task task) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("10.6.4.172");
+        //factory.setPort(15672);
+        factory.setUsername("root");
+        factory.setPassword("root");
+        logger.info("RabbitMQ: ConnectionFactory set on 10.6.4.172:15672");
+        try {
+            Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            logger.info("RabbitMQ: queque created");
             channel.basicPublish("", QUEUE_NAME, null, task.toString().getBytes());
             channel.close();
+            logger.info("RabbitMQ: channel closed");
+            connection.close();
+            logger.info("RabbitMQ: connection closed");
             //TODO: Task powinien być serializowalny, żebym mógł go prawidłowo podawać do kolejki
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (IOException | TimeoutException e) {
             e.printStackTrace();
         }
+
     }
 }
